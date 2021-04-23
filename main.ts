256b224cf4539631f5a476cd1e5b0c713d64504e
just1ngray <just1ngray@outlook.com>
2021-04-23 13:21:50 -0300

    created a simple key file

--- main.ts
+++ main.ts
@@ -1,6 +1,21 @@
+import fs from "fs";
+
 import * as database from "./database/db";
 import scraperWorker from "./scrapers/worker";
 
+// set up key file
+if (!fs.existsSync("./keys.ts")) fs.writeFileSync("./keys.ts",
+`export default {
+    google: {
+        geocode: "GOOGLE_GEOCODE_API_KEY"
+    },
+    mailjet: {
+        key: "MAILJET_MAIN_ACCOUNT_API_KEY",
+        secret: "MAILJET_MAIN_ACCOUNT_SECRET_KEY"
+    }
+};`);
+
+// connect to the database and start tasks
 (async () => {
     await database.connect();
     process.on("SIGINT", async () => {
