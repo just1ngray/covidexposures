7a5c87519851478996d23c467a6fe043a90ebdf6
just1ngray <just1ngray@outlook.com>
2021-04-24 22:42:07 -0300

    placeholder todos

--- pages/status.tsx
+++ pages/status.tsx
@@ -1,23 +1,37 @@
+import Container from "../components/Container";
 import { ScraperModel, Scraper } from "../database/Scraper";
 import * as db from "../database/db";
 
-export default function Status({ scrapers }) {
+interface Props {
+    scrapers: Scraper[],
+    updated: number
+}
+
+export default function Status({ scrapers, updated }: Props) {
+    const updatedFormatted = (new Date(updated)).toLocaleTimeString();
+
     return (
-        <>
-            <h2>Status: </h2>
-            <pre>{JSON.stringify(scrapers, null, 2)}</pre>
-        </>
+        <Container>
+            <h2>Status:</h2>
+            {/* <pre>{JSON.stringify(scrapers, null, 2)}</pre> */}
+
+            <p>Updated {updatedFormatted}</p>
+        </Container>
     );
 }
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
     await db.connect();
-    const scrapers = await ScraperModel.find();
+    const scrapers = await ScraperModel.find()
+        .select("-_id -__v")
+        .lean() as Scraper[];
     await db.disconnect();
 
-    console.log(scrapers);
-
     return {
-        props: { scrapers: scrapers.map(s => s.name) }
+        props: { 
+            scrapers,
+            updated: Date.now()
+        },
+        revalidate: 5*60 // 5 minutes
     }
 }
