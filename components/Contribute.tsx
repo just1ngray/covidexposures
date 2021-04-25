5a90f89074c94bce7183be553eb0c987399997a3
just1ngray <just1ngray@outlook.com>
2021-04-25 20:29:27 -0300

    contribute to the project

---
+++ components/Contribute.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import PopButton from "./PopButton";
+
+interface Props {
+    message?: string,
+    href?: string,
+    className?: string
+}
+
+export default function Contribute({ 
+    message = "Help make this open source app better!", 
+    href = "https://github.com/just1ngray/covidexposures",
+    className = ""
+}: Props) {
+    return (
+        <div className={`flex justify-center ${className}`}>
+            <PopButton className="
+                flex items-center
+                border-gray-100 border
+                p-2 rounded-md
+                text-left
+            " onClick={() => window.location.href=href}>
+                <div className="inline-block">
+                    <Image src="/GitHub-Mark-64px.png" layout="fixed" height={64} width={64} />
+                </div>
+                <div className="pl-2">
+                    <p className="text-xl font-semibold">Contribute</p>
+                    <p className="">{message}</p>
+                </div>
+            </PopButton>
+        </div>
+    );
+}
