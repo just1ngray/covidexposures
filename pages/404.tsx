408bfbd2c66c4476c19c30619d6e4702904de645
just1ngray <just1ngray@outlook.com>
2021-04-24 22:42:30 -0300

    created 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,11 @@
+import Container from "../components/Container";
+
+export default function Custom404() {
+    return (
+        <Container>
+            <p className="text-center py-24">
+                The page you are looking for cannot be found.
+            </p>
+        </Container>
+    );
+}
