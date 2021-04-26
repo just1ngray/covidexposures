f006414cd173966b45645fa176c93e93b8071aed
just1ngray <just1ngray@outlook.com>
2021-04-26 17:48:46 -0300

    allow passing classes to the main container

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,15 +1,16 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 
-export default function Container({ children }) {
+export default function Container({ className, children }) {
     return (
         <div className="flex flex-col h-screen">
             <Navbar />
-            <main className="
+            <main className={`
                 flex flex-col 
                 flex-grow container mx-auto px-2 
                 relative
-            ">
+                ${className}
+            `}>
                 {children}
             </main>
             <Footer />
