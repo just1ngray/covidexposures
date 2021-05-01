b22b829ce916a77fea885217378407187b590eef
just1ngray <just1ngray@outlook.com>
2021-05-01 15:14:32 -0300

    add pagination to popup

--- components/pagewise/heatmap/MoreInfo.tsx
+++ components/pagewise/heatmap/MoreInfo.tsx
@@ -1,19 +1,27 @@
 import { Popup } from "react-map-gl";
+import { useState } from "react";
+
 import { Exposure } from "../../../database/Exposure";
 
-interface Props {
-    /* overlapping exposure */
-    exposure: Exposure & { all: {
+interface Timeframes {
+    all: {
         start: number,
         end: number,
         instructions: string
-    }[] },
+    }[]
+}
+
+interface Props {
+    /* overlapping exposure */
+    exposure: Exposure & Timeframes,
     close: () => void
 }
 
 export default function MoreInfo({ exposure, close }: Props) {
+    console.log(exposure);
+
     return (
-        <Popup closeOnClick={true}
+        <Popup closeOnClick={false}
             onClose={close}
             longitude={exposure.coord.long}
             latitude={exposure.coord.lat}
@@ -27,7 +35,7 @@ export default function MoreInfo({ exposure, close }: Props) {
                     </a>
                 </div>
 
-                <ol className="text-xs text-gray-600 tracking-tight">
+                {/* <ol className="text-xs text-gray-600 tracking-tight">
                     {exposure.all.map((doc, i) => 
                         <li key={i} className="mb-2">
                             <span className="inline-block align-text-top">
@@ -38,8 +46,65 @@ export default function MoreInfo({ exposure, close }: Props) {
                             </span>
                         </li>
                     )}
-                </ol>
+                </ol> */}
+                <TimeframePaginate perPage={4} times={exposure} />
+
             </div>
         </Popup>
+    );
+}
+
+interface ExposurePageProps {
+    times: Timeframes,
+    perPage: number
+}
+
+function TimeframePaginate({ times, perPage }: ExposurePageProps) {
+    const [page, setPage] = useState(0);
+
+    const toShow = times.all.slice(page * perPage, page * perPage + perPage);
+    const maxPages = Math.ceil(times.all.length / perPage) - 1;
+
+    return (
+        <div>
+            {toShow.map((e,i) =>
+                <div key={i} className="align-text-top mb-2">
+                    {new Date(e.start).toLocaleString()}
+                    {" to "}
+                    {new Date(e.end).toLocaleString()}
+                    <p className="text-gray-400">{e.instructions}</p>
+                </div>
+            )}
+            
+            {maxPages > 1 && 
+                <div className="flex justify-center">
+                    <button type="button" 
+                        disabled={page <= 0} 
+                        onClick={() => setPage(Math.max(0, page - 1))}
+                        className="
+                            border border-gray-400 rounded-l
+                            p-1.5 w-24
+                            disabled:opacity-40 disabled:cursor-not-allowed
+                            hover:bg-gray-100
+                    ">
+                        {"< Previous"}
+                    </button>
+                    <span className="border-t border-b border-gray-400 p-1.5 w-16">
+                        {page+1}/{maxPages+1}
+                    </span>
+                    <button type="button" 
+                        disabled={page >= maxPages} 
+                        onClick={() => setPage(Math.min(maxPages, page + 1))}
+                        className="
+                            border border-gray-400 rounded-r
+                            p-1.5 w-24
+                            disabled:opacity-40 disabled:cursor-not-allowed
+                            hover:bg-gray-100
+                    ">
+                        {"Next >"}
+                    </button>
+                </div>
+            }
+        </div>
     );
 }
