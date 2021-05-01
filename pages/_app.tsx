6d1a4deac8e5cd873f59dc088ee429b332804357
just1ngray <just1ngray@outlook.com>
2021-05-01 19:59:16 -0300

    add site-wide head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
 import { AppProps } from "next/app";
+import Head from "next/head";
 
 import "tailwindcss/tailwind.css";
 import "../util/frontend/global.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+    return (
+        <>
+            <Head>
+                <link rel="shortcut icon" href="/logo.png" />
+                <title>COVID Exposures</title>
+                <meta name="description" content="Data driven COVID Exposures and Notifications"></meta>
+                <meta name="keywords" content="COVID, Coronavirus, Exposures, COVID Exposures"></meta>
+            </Head>
+
+            <Component {...pageProps} />
+        </>
+    );
 }
