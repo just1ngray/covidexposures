412fc53b8a30f90c6bccab7558c7910b99b6b96a
just1ngray <just1ngray@outlook.com>
2021-05-01 19:36:14 -0300

    clarify the landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,43 +3,39 @@ import Link from "next/link";
 
 import Container from '../components/Container';
 import Contribute from "../components/pagewise/status/Contribute";
+import PopButton from "../components/PopButton";
 
 export default function Home() {
     return (
         <Container>
-            <h2 className="text-center font-bold text-5xl mb-2">COVID Exposures - driven by technology.</h2>
-
+            <h2 className="text-center font-bold text-5xl my-2">
+                COVID Exposures - driven by technology.
+            </h2>
             <p className="text-sm text-center text-gray-600 mb-6">
                 Not affiliated with or endorsed by any company or government organization.
             </p>
 
-            <Feature src="/heatmap-high.png" height={1574} width={1996}>
-                <p>
-                    Browse a region by its publically announced exposures.
-                </p>
-            </Feature>
-
-            <Contribute message="100% open source - add your local health authority!" />
-
-            <Feature src="/heatmap-details.png" height={1738} width={1996} text="LEFT">
-                <p>
-                    Explore specific exposures in detail by zooming in.
-                </p>
-            </Feature>
-
             <p className="text-center text-lg">
-                Tired of manually checking your local health authority's potential exposure list?
+                Protecting you with up-to-date notifications of your potential exposures.
             </p>
 
-            <Feature src="/subscriptions.png" height={624} width={1504}>
+            <Feature src="/subscriptions.png" height={624/2} width={1504/2} href="/login">
                 <p className="md:max-w-sm">
                     Sign in to add your locations. You'll automatically receive email notifications
                     if a potential exposure is detected within your subscription region.
                 </p>
             </Feature>
 
+            <Contribute message="100% open source - add your local health authority!" />
+
+            <Feature src="/heatmap-high.png" height={1574/4} width={1996/4} text="LEFT" href="/heatmap">
+                <p>
+                    Or explore exposures by their geographic region with our Hot Spots feature.
+                </p>
+            </Feature>
+
             <div className="text-center mt-4 mb-12">
-                We have a very simple{` `}
+                We have a simple{` `}
                 <Link href="/privacy"><a className="text-blue-500 underline">Privacy Policy</a></Link>
                 {` and `}
                 <Link href="/terms"><a className="text-blue-500 underline">Terms of Service</a></Link>.<br />
@@ -61,25 +57,31 @@ interface FeatureProps {
     height: number,
     width: number,
     children: any,
+    href: string,
     text?: "RIGHT" | "LEFT"
 }
 
-function Feature({ src, height, width, children, text="RIGHT" }: FeatureProps) {
+function Feature({ src, height, width, children, text="RIGHT", href }: FeatureProps) {
     return (
-        <div className="shadow-lg p-4 mb-4
-            md:flex
-        ">
-            <div className="order-1">
-                <Image src={src} height={height} width={width} />
-            </div>
+        <Link href={href}>
+            <PopButton className="shadow-lg p-4 mb-4
+                md:flex md:items-center md:justify-evenly
+            ">
+                <div className="order-1 md:w-3/4">
+                    <Image src={src} 
+                        quality={5} priority
+                        height={height} width={width}
+                    />
+                </div>
 
-            <div className={
-                `md:p-4 md:flex md:items-center 
-                ${text == "LEFT" ? "order-0" : "order-2"}
-                text-lg text-gray-700
-            `}>
-                {children}
-            </div>
-        </div>
+                <div className={
+                    `md:p-4 md:w-1/4
+                    ${text == "LEFT" ? "order-0" : "order-2"}
+                    text-lg text-gray-700 
+                `}>
+                    {children}
+                </div>
+            </PopButton>
+        </Link>
     );
 }
