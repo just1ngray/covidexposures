a658c9c36d251a68e0a16544edaaf34ea02ccffd
just1ngray <just1ngray@outlook.com>
2021-05-08 17:50:15 -0300

    add subscription coord index

--- src/database/Subscription.ts
+++ src/database/Subscription.ts
@@ -76,4 +76,6 @@ export const SubscriptionSchema = new Schema({
     }
 });
 
+SubscriptionSchema.index({ coord: "2dsphere" });
+
 export const SubscriptionModel = models.Subscription || model("Subscription", SubscriptionSchema);
