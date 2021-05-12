7994a1a9e88bf494dcf6080df73695c98be63b95
just1ngray <just1ngray@outlook.com>
2021-05-12 00:20:43 -0300

    fix STUPID 403 mapbox geocode error

--- src/backend/geocode.ts
+++ src/backend/geocode.ts
@@ -1,5 +1,5 @@
-import mbxGeocode from "@mapbox/mapbox-sdk/services/geocoding";
 import { LeanDocument } from "mongoose";
+import axios from "axios";
 
 import keys from "../../keys";
 import { ExposureModel, Exposure } from "../database/Exposure";
@@ -10,26 +10,26 @@ export default async function geocode(
     exposure: ScrapedExposure, 
     scraper: Scraper
 ): Promise<{ long: number, lat: number }> {
-    const geocodeService = mbxGeocode({ accessToken: keys.mapbox });
-
     const candidates = await ExposureModel.find({ 
         address: exposure.address, 
         scraper: scraper._id
     }).lean() as LeanDocument<Exposure>[];
     if (candidates.length > 0) return candidates[0].coord;
 
-    const { body } = await geocodeService.forwardGeocode({
-        mode: "mapbox.places",
-        query: exposure.address,
-        countries: scraper.country.split(","),
-        limit: 1,
-        language: scraper.language.split(","),
-        proximity: [scraper.center.long, scraper.center.lat]
-    }).send();
+    const { data } = await axios.get(
+        `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(exposure.address)}.json`, {
+        params: {
+            access_token: keys.mapbox,
+            countries: scraper.country,
+            limit: 1,
+            language: scraper.language,
+            proximity: [scraper.center.long, scraper.center.lat]
+        }
+    });
 
-    if (body.features.length == 0) throw new Error("invalid address")
+    if (data.features.length == 0) throw new Error("invalid address")
     return {
-        long: body.features[0].center[0],
-        lat: body.features[0].center[1]
+        long: data.features[0].center[0],
+        lat: data.features[0].center[1]
     };
 }
